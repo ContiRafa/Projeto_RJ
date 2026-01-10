3a8059ee5b690bc55b03804b31570a806319bc64
Rafael Contieri <155206195+ContiRafa@users.noreply.github.com>
2026-01-09 22:09:08 -0300

    testando

---
+++ Projeto_RJ/telaInicial.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Projeto_RJ
+{
+    partial class telaInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DateTimePicker dateTimePicker2;
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_name_header = new System.Windows.Forms.Label();
+            dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.lbl_name_header);
+            this.panel1.Controls.Add(dateTimePicker2);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1902, 85);
+            this.panel1.TabIndex = 8;
+            // 
+            // lbl_name_header
+            // 
+            this.lbl_name_header.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_name_header.AutoSize = true;
+            this.lbl_name_header.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_name_header.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_name_header.Location = new System.Drawing.Point(9, 24);
+            this.lbl_name_header.Name = "lbl_name_header";
+            this.lbl_name_header.Size = new System.Drawing.Size(205, 29);
+            this.lbl_name_header.TabIndex = 5;
+            this.lbl_name_header.Text = "TOTEM VIRTUAL";
+            this.lbl_name_header.Click += new System.EventHandler(this.lbl_name_header_Click);
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            dateTimePicker2.CalendarForeColor = System.Drawing.SystemColors.ButtonFace;
+            dateTimePicker2.CalendarMonthBackground = System.Drawing.Color.DarkOrange;
+            dateTimePicker2.CalendarTitleForeColor = System.Drawing.SystemColors.ButtonFace;
+            dateTimePicker2.CalendarTrailingForeColor = System.Drawing.SystemColors.ButtonFace;
+            dateTimePicker2.Enabled = false;
+            dateTimePicker2.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Bold);
+            dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new System.Drawing.Point(1669, 24);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new System.Drawing.Size(222, 33);
+            dateTimePicker2.TabIndex = 6;
+            // 
+            // telaInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.panel1);
+            this.Name = "telaInicial";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_name_header;
+    }
+}
