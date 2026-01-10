065a1a45f5bb46896a5174248771095c778f90de
Rafael Contieri <155206195+ContiRafa@users.noreply.github.com>
2026-01-09 22:28:47 -0300

    commit com data

--- Projeto_RJ/frmInicial.Designer.cs
+++ Projeto_RJ/frmInicial.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicial));
+            System.Windows.Forms.DateTimePicker dateTimePicker2;
             this.lbl_name_header = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnIniciar = new System.Windows.Forms.Button();
+            dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -45,7 +47,7 @@
             this.lbl_name_header.AutoSize = true;
             this.lbl_name_header.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_name_header.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.lbl_name_header.Location = new System.Drawing.Point(9, 24);
+            this.lbl_name_header.Location = new System.Drawing.Point(9, 29);
             this.lbl_name_header.Name = "lbl_name_header";
             this.lbl_name_header.Size = new System.Drawing.Size(205, 29);
             this.lbl_name_header.TabIndex = 5;
@@ -57,6 +59,7 @@
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.panel1.Controls.Add(this.lbl_name_header);
+            this.panel1.Controls.Add(dateTimePicker2);
             this.panel1.Location = new System.Drawing.Point(2, 1);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1262, 85);
@@ -68,7 +71,7 @@
             this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Controls.Add(this.btnIniciar);
-            this.panel2.Location = new System.Drawing.Point(378, 135);
+            this.panel2.Location = new System.Drawing.Point(335, 105);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(600, 432);
             this.panel2.TabIndex = 8;
@@ -95,6 +98,24 @@
             this.btnIniciar.Text = "TOQUE PARA INICIAR";
             this.btnIniciar.UseVisualStyleBackColor = false;
             this.btnIniciar.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dateTimePicker2.CalendarForeColor = System.Drawing.SystemColors.ButtonFace;
+            dateTimePicker2.CalendarMonthBackground = System.Drawing.Color.DarkOrange;
+            dateTimePicker2.CalendarTitleForeColor = System.Drawing.SystemColors.ButtonFace;
+            dateTimePicker2.CalendarTrailingForeColor = System.Drawing.SystemColors.ButtonFace;
+            dateTimePicker2.Enabled = false;
+            dateTimePicker2.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Bold);
+            dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new System.Drawing.Point(1028, 27);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new System.Drawing.Size(222, 33);
+            dateTimePicker2.TabIndex = 6;
+            dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
             // 
             // frmInicial
             // 
