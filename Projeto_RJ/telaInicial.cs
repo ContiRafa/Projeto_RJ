8336b8c9b7768ae184f313397408353347d1c50b
Rafael Contieri <155206195+ContiRafa@users.noreply.github.com>
2026-01-10 00:06:16 -0300

    cm_2

--- Projeto_RJ/telaInicial.cs
+++ Projeto_RJ/telaInicial.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -39,7 +40,7 @@ namespace Projeto_RJ
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
